441a40f8cd7ef863bd1ac344abfee2e96b95b352
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2022-06-03 15:43:27 +0530

    Added Compose components

---
+++ app/src/main/java/com/ashlesh/fluffies/component/ItemCatCard.kt
@@ -0,0 +1,102 @@
+package com.ashlesh.fluffies.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.ashlesh.fluffies.R
+import com.ashlesh.fluffies.model.Cat
+
+@Composable
+fun ItemCatCard(cat: Cat, onItemClicked: (cat: Cat) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(onClick = { onItemClicked(cat) }),
+        elevation = 0.dp,
+        backgroundColor = MaterialTheme.colors.onSurface
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            val image: Painter = painterResource(id = cat.image)
+            Image(
+                modifier = Modifier
+                    .size(80.dp, 80.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                painter = image,
+                alignment = Alignment.CenterStart,
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                Text(
+                    text = cat.name,
+                    modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp),
+                    color = MaterialTheme.colors.surface,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = buildString {
+                        append(cat.age)
+                        append("yrs | ")
+                        append(cat.gender)
+                    },
+                    modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp),
+                    color = MaterialTheme.colors.surface,
+                    style = MaterialTheme.typography.caption
+                )
+
+                Row(verticalAlignment = Alignment.Bottom) {
+
+                    val location: Painter = painterResource(id = R.drawable.ic_location)
+
+                    Icon(
+                        painter = location,
+                        contentDescription = null,
+                        modifier = Modifier.size(16.dp, 16.dp),
+                        tint = Color.Red
+                    )
+
+                    Text(
+                        text = cat.location,
+                        modifier = Modifier.padding(8.dp, 12.dp, 12.dp, 0.dp),
+                        color = MaterialTheme.colors.surface,
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                GenderTag(cat.gender)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ashlesh/fluffies/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.ashlesh.fluffies.navigation
+
+import androidx.annotation.StringRes
+import com.ashlesh.fluffies.R
+
+sealed class Screen(val route: String, @StringRes val resourceId: Int) {
+    object Home : Screen("home", R.string.text_home)
+    object Details : Screen("details", R.string.text_details)
+}
